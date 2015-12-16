2096aefda925b742b7874b5353db8bf5bf16e316
banes <djex808@gmail.com>
2015-12-16 17:09:17 +0100

    FInished app frontend, just some refining left to do.

--- js/custom.js
+++ js/custom.js
@@ -31,5 +31,56 @@ jQuery(document).ready(function ($) {
             });
     });
 
+    /*
+     * Replace all SVG images with inline SVG
+     */
+    jQuery('img.svg').each(function () {
+        var $img = jQuery(this);
+        var imgID = $img.attr('id');
+        var imgClass = $img.attr('class');
+        var imgURL = $img.attr('src');
+
+        jQuery.get(imgURL, function (data) {
+            // Get the SVG tag, ignore the rest
+            var $svg = jQuery(data).find('svg');
+
+            // Add replaced image's ID to the new SVG
+            if (typeof imgID !== 'undefined') {
+                $svg = $svg.attr('id', imgID);
+            }
+            // Add replaced image's classes to the new SVG
+            if (typeof imgClass !== 'undefined') {
+                $svg = $svg.attr('class', imgClass + ' replaced-svg');
+            }
+
+            // Remove any invalid XML tags as per http://validator.w3.org
+            $svg = $svg.removeAttr('xmlns:a');
+
+            // Replace image with new SVG
+            $img.replaceWith($svg);
+
+        }, 'xml');
+
+    });
+
+
+    // add class active on restaurant buttons
+    $('.restaurant-button').on('click', function () {
+        $('.restaurant-button').removeClass('active');
+        $(this).addClass('active');
+    });
+
+    <!-- initialize the calendar on ready -->
+    $("#calendar").zabuto_calendar({
+        today: true
+    });
+
+    // hide footer on input focus
+    $('input, textarea').blur(function() {
+            $('.footer-nav').removeClass("focus");
+        })
+        .focus(function() {
+            $('.footer-nav').addClass("focus")
+        });
 
 });
